f83f1e65bdb01a32926a073e42da8e5a78a9bce0
Parth0231 <patilparth14@gmail.com>
2020-09-16 18:15:24 +0530

    Add files via upload

---
+++ Backend/ThinkBridge_unitTest/DummyDataDBInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ThinkBridge_Api.Models;
+namespace ThinkBridge_unitTest
+{
+    public class DummyDataDBInitializer
+    {
+        public DummyDataDBInitializer()
+        {
+        }
+
+        public void Seed(ThinkBridgeDBContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            context.Inventory.AddRange(
+                new Inventory() { Name = "candy", Description = "candycandycandy", Price= 10,Image=null },
+                new Inventory() { Name = "biscuit", Description = "biscuitbiscuit",Price = 20, Image = null },
+                new Inventory() { Name = "toolkit", Description = "toolkittoolkit", Price = 30, Image = null },
+                new Inventory() { Name = "tape", Description = "tapetapetapetapetape", Price = 40, Image = null }
+            );           
+            context.SaveChanges();
+        }
+    }
+}
